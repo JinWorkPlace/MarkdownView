2ce2d6649d5f753c6722c85e35ff47f40a447b29
doanvu20122000 <doanvu20122000@gmail.com>
2025-07-01 15:49:45 +0700

    Add Markdown support and update UI components in MainActivity

--- app/src/main/java/com/apps/markdown/MainActivity.kt
+++ app/src/main/java/com/apps/markdown/MainActivity.kt
@@ -10,7 +10,23 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun initView() {
+//        binding.edtInput.doAfterTextChanged { editable ->
+//            binding.tvResult.text = editable.toString()
+//        }
 
+        io.noties.markwon.Markwon.create(this).apply {
+            val markdown = """
+                # Hello World
+                This is a **Markdown** example.
+                
+                - Item 1
+                - Item 2
+                - Item 3
+                
+                [Click here](https://www.example.com) to visit example.com.
+            """.trimIndent()
+            binding.tvResult.text = this.toMarkdown(markdown)
+        }
     }
 
     override fun initData() {
