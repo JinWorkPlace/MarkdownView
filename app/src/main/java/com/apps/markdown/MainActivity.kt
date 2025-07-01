550ece81152aa635e48289e569c86166fa3b1e99
doanvu20122000 <doanvu20122000@gmail.com>
2025-07-01 16:07:10 +0700

    Enhance Markdown input handling in MainActivity

--- app/src/main/java/com/apps/markdown/MainActivity.kt
+++ app/src/main/java/com/apps/markdown/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.apps.markdown
 
 import android.view.LayoutInflater
+import androidx.core.widget.doAfterTextChanged
 import com.apps.markdown.base.BaseActivity
 import com.apps.markdown.databinding.ActivityMainBinding
+import io.noties.markwon.Markwon
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun inflateViewBinding(inflater: LayoutInflater): ActivityMainBinding {
@@ -10,23 +12,26 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun initView() {
-//        binding.edtInput.doAfterTextChanged { editable ->
-//            binding.tvResult.text = editable.toString()
-//        }
-
-        io.noties.markwon.Markwon.create(this).apply {
-            val markdown = """
-                # Hello World
-                This is a **Markdown** example.
-                
-                - Item 1
-                - Item 2
-                - Item 3
-                
-                [Click here](https://www.example.com) to visit example.com.
-            """.trimIndent()
-            binding.tvResult.text = this.toMarkdown(markdown)
+        val markDown = Markwon.create(this)
+        binding.edtInput.doAfterTextChanged { editable ->
+            val input = editable.toString()
+            val result = markDown.toMarkdown(input)
+            binding.tvResult.text = result
         }
+
+//        io.noties.markwon.Markwon.create(this).apply {
+//            val markdown = """
+//                # Hello World
+//                This is a **Markdown** example.
+//
+//                - Item 1
+//                - Item 2
+//                - Item 3
+//
+//                [Click here](https://www.example.com) to visit example.com.
+//            """.trimIndent()
+//            binding.tvResult.text = this.toMarkdown(markdown)
+//        }
     }
 
     override fun initData() {
