8341c880562cb795d8f6b6eba3060bb68b8e5119
HoiCham <p.n.thang0101@gmail.com>
2025-07-02 21:23:34 +0700

    Add utility classes for node dumping and layout handling

--- markwon-core/src/main/java/io/noties/markwon/utils/DumpNodes.kt
+++ markwon-core/src/main/java/io/noties/markwon/utils/DumpNodes.kt
@@ -1,119 +1,93 @@
-package io.noties.markwon.utils;
+package io.noties.markwon.utils
 
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import org.commonmark.node.Node;
-import org.commonmark.node.Visitor;
-
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import androidx.annotation.CheckResult
+import org.commonmark.node.Node
+import org.commonmark.node.Visitor
+import java.lang.reflect.Proxy
 
 // utility class to print parsed Nodes hierarchy
-@SuppressWarnings({"unused", "WeakerAccess"})
-public abstract class DumpNodes {
+@Suppress("unused")
+object DumpNodes {
+    @CheckResult
+    fun dump(node: Node): String {
+        return dump(node, null)
+    }
+
+    @CheckResult
+    fun dump(node: Node, nodeProcessor: NodeProcessor?): String {
+        val processor = nodeProcessor ?: NodeProcessorToString()
+
+        val indent = Indent()
+        val builder = StringBuilder()
+        val visitor = Proxy.newProxyInstance(
+            Visitor::class.java.classLoader, arrayOf<Class<*>>(Visitor::class.java)
+        ) { proxy, method, args ->
+            val argument = args[0] as Node
+
+            // initial indent
+            indent.appendTo(builder)
+
+            // node info
+            builder.append(processor.process(argument))
+
+            // @since 4.6.0 check for first child instead of casting to Block
+            //  (regular nodes can contain other nodes, for example Text)
+            if (argument.firstChild != null) {
+                builder.append(" [\n")
+                indent.increment()
+                visitChildren((proxy as Visitor?)!!, argument)
+                indent.decrement()
+                indent.appendTo(builder)
+                builder.append("]\n")
+            } else {
+                builder.append("\n")
+            }
+
+            null
+        } as Visitor
+        node.accept(visitor)
+        return builder.toString()
+    }
+
+    private fun visitChildren(visitor: Visitor, parent: Node) {
+        var node = parent.firstChild
+        while (node != null) {
+            // A subclass of this visitor might modify the node, resulting in getNext returning a different node or no
+            // node after visiting it. So get the next node before visiting.
+            val next = node.next
+            node.accept(visitor)
+            node = next
+        }
+    }
 
     /**
      * Creates String representation of a node which will be used in output
      */
-    public interface NodeProcessor {
-        @NonNull
-        String process(@NonNull Node node);
+    interface NodeProcessor {
+        fun process(node: Node): String
     }
 
-    @NonNull
-    @CheckResult
-    public static String dump(@NonNull Node node) {
-        return dump(node, null);
-    }
+    private class Indent {
+        private var count = 0
 
-    @NonNull
-    @CheckResult
-    public static String dump(@NonNull Node node, @Nullable NodeProcessor nodeProcessor) {
-
-        final NodeProcessor processor = nodeProcessor != null
-                ? nodeProcessor
-                : new NodeProcessorToString();
-
-        final Indent indent = new Indent();
-        final StringBuilder builder = new StringBuilder();
-        final Visitor visitor = (Visitor) Proxy.newProxyInstance(
-                Visitor.class.getClassLoader(),
-                new Class[]{Visitor.class},
-                new InvocationHandler() {
-                    @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) {
-
-                        final Node argument = (Node) args[0];
-
-                        // initial indent
-                        indent.appendTo(builder);
-
-                        // node info
-                        builder.append(processor.process(argument));
-
-                        // @since 4.6.0 check for first child instead of casting to Block
-                        //  (regular nodes can contain other nodes, for example Text)
-                        if (argument.getFirstChild() != null) {
-                            builder.append(" [\n");
-                            indent.increment();
-                            visitChildren((Visitor) proxy, argument);
-                            indent.decrement();
-                            indent.appendTo(builder);
-                            builder.append("]\n");
-                        } else {
-                            builder.append("\n");
-                        }
-
-                        return null;
-                    }
-                });
-        node.accept(visitor);
-        return builder.toString();
-    }
-
-    private DumpNodes() {
-    }
-
-    private static class Indent {
-
-        private int count;
-
-        void increment() {
-            count += 1;
+        fun increment() {
+            count += 1
         }
 
-        void decrement() {
-            count -= 1;
+        fun decrement() {
+            count -= 1
         }
 
-        void appendTo(@NonNull StringBuilder builder) {
-            for (int i = 0; i < count; i++) {
-                builder
-                        .append(' ')
-                        .append(' ');
+        fun appendTo(builder: StringBuilder) {
+            for (i in 0..<count) {
+                builder.append(' ').append(' ')
             }
         }
     }
 
-    private static void visitChildren(@NonNull Visitor visitor, @NonNull Node parent) {
-        Node node = parent.getFirstChild();
-        while (node != null) {
-            // A subclass of this visitor might modify the node, resulting in getNext returning a different node or no
-            // node after visiting it. So get the next node before visiting.
-            Node next = node.getNext();
-            node.accept(visitor);
-            node = next;
-        }
-    }
-
-    private static class NodeProcessorToString implements NodeProcessor {
-        @NonNull
-        @Override
-        public String process(@NonNull Node node) {
-            return node.toString();
+    private class NodeProcessorToString : NodeProcessor {
+        override fun process(node: Node): String {
+            return node.toString()
         }
     }
 }
--- markwon-core/src/main/java/io/noties/markwon/utils/LayoutUtils.kt
+++ markwon-core/src/main/java/io/noties/markwon/utils/LayoutUtils.kt
@@ -1,64 +1,54 @@
-package io.noties.markwon.utils;
+package io.noties.markwon.utils
 
-import android.text.Layout;
-
-import androidx.annotation.NonNull;
+import android.text.Layout
 
 /**
  * @since 4.4.0
  */
-public abstract class LayoutUtils {
+object LayoutUtils {
+    private const val DEFAULT_EXTRA = 0F
+    private const val DEFAULT_MULTIPLIER = 1F
 
-    private static final float DEFAULT_EXTRA = 0F;
-    private static final float DEFAULT_MULTIPLIER = 1F;
+    fun getLineBottomWithoutPaddingAndSpacing(layout: Layout, line: Int): Int {
+        val bottom = layout.getLineBottom(line)
+        val isSpanLastLine = line == layout.lineCount - 1
+        val lineSpacingExtra = layout.spacingAdd
+        val lineSpacingMultiplier = layout.spacingMultiplier
 
-    public static int getLineBottomWithoutPaddingAndSpacing(@NonNull Layout layout, int line) {
+        // Simplified check
+        val hasLineSpacing =
+            lineSpacingExtra != DEFAULT_EXTRA || lineSpacingMultiplier != DEFAULT_MULTIPLIER
 
-        final int bottom = layout.getLineBottom(line);
-        final boolean lastLineSpacingNotAdded = true;
-        final boolean isSpanLastLine = line == (layout.getLineCount() - 1);
-
-        final int lineBottom;
-        final float lineSpacingExtra = layout.getSpacingAdd();
-        final float lineSpacingMultiplier = layout.getSpacingMultiplier();
-
-        // simplified check
-        final boolean hasLineSpacing = lineSpacingExtra != DEFAULT_EXTRA || lineSpacingMultiplier != DEFAULT_MULTIPLIER;
-
-        if (!hasLineSpacing || isSpanLastLine) {
-            lineBottom = bottom;
+        val lineBottom = if (!hasLineSpacing || isSpanLastLine) {
+            bottom
         } else {
-            final float extra;
-            if (Float.compare(DEFAULT_MULTIPLIER, lineSpacingMultiplier) != 0) {
-                final int lineHeight = getLineHeight(layout, line);
-                extra = lineHeight - ((lineHeight - lineSpacingExtra) / lineSpacingMultiplier);
+            val extra = if (DEFAULT_MULTIPLIER.compareTo(lineSpacingMultiplier) != 0) {
+                val lineHeight = getLineHeight(layout, line)
+                lineHeight - ((lineHeight - lineSpacingExtra) / lineSpacingMultiplier)
             } else {
-                extra = lineSpacingExtra;
+                lineSpacingExtra
             }
-            lineBottom = (int) (bottom - extra + .5F);
+            (bottom - extra + 0.5F).toInt()
         }
 
-        // check if it is the last line that span is occupying **and** that this line is the last
-        //  one in TextView
-        if (isSpanLastLine && (line == layout.getLineCount() - 1)) {
-            return lineBottom - layout.getBottomPadding();
+        // Check if it is the last line that span is occupying and that this line is the last one in TextView
+        return if (isSpanLastLine && line == layout.lineCount - 1) {
+            lineBottom - layout.bottomPadding
+        } else {
+            lineBottom
         }
-
-        return lineBottom;
     }
 
-    public static int getLineTopWithoutPadding(@NonNull Layout layout, int line) {
-        final int top = layout.getLineTop(line);
-        if (line == 0) {
-            return top - layout.getTopPadding();
+    fun getLineTopWithoutPadding(layout: Layout, line: Int): Int {
+        val top = layout.getLineTop(line)
+        return if (line == 0) {
+            top - layout.topPadding
+        } else {
+            top
         }
-        return top;
     }
 
-    public static int getLineHeight(@NonNull Layout layout, int line) {
-        return layout.getLineTop(line + 1) - layout.getLineTop(line);
-    }
-
-    private LayoutUtils() {
+    fun getLineHeight(layout: Layout, line: Int): Int {
+        return layout.getLineTop(line + 1) - layout.getLineTop(line)
     }
 }
